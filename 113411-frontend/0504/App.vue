ffd8a5a6f193fc4aeb83e7a75cc69243155b807d
11046072yongjie <147406854+yongjie0328@users.noreply.github.com>
2024-05-05 16:07:26 +0800

    Add files via upload

---
+++ 113411-frontend/0504/App.vue
@@ -0,0 +1,37 @@
+<template>
+   <div id="app">
+    <Navbar v-if="!$route.meta.hideNavbar" />
+    <router-view/>
+    <Footer v-if="!$route.meta.hideFooter" />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  align-items: center;
+
+  
+}
+main {
+  flex: 1; /* 强制main区域填充可用空间 */
+}
+
+</style>
