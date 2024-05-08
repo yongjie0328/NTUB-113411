2b30b31bebb5b6cf48bf5d1c58a0bb22b6cb1ea9
yinghsiu0701 <94967161+yinghsiu0701@users.noreply.github.com>
2024-05-08 23:25:51 +0800

    Add files via upload

--- 113411-backend/stock_info/stock_info.py
+++ 113411-backend/stock_info/stock_info.py
@@ -33,7 +33,7 @@ try:
         # 選擇市場類型
         market_select.select_by_visible_text(market_type)
 
-        # 顯示等待市場類型選項加載完成
+        # 顯示資料載入中，請稍後..
         wait.until(EC.presence_of_element_located((By.XPATH, "//select[@name='code']//option")))
 
         # 找到產業類型下拉選單
@@ -48,11 +48,11 @@ try:
 
         # 等待查詢結果加載完成
         time.sleep(10)  # 這裡等待10秒，可以根據頁面加載速度進行調整
-      
-        # 假設html_content是你獲取到的HTML原始碼
+
+        # 獲取網頁的原始碼
         html_content = driver.page_source
 
-        # 使用BeautifulSoup解析HTML
+        # 使用BeautifulSoup模組解析網頁
         soup = BeautifulSoup(html_content, 'html.parser')
 
         if market_type == '上市':
@@ -72,7 +72,7 @@ try:
                             "簽證會計師1", "簽證會計師2", "英文簡稱", "英文通訊地址", "傳真機號碼", "電子郵件信箱", "公司網址", "投資人關係聯絡人",
                             "投資人關係聯絡人職稱", "投資人關係聯絡電話", "投資人關係聯絡電子郵件", "公司網站內利害關係人專區網址"]
 
-        # 創建一個空列表來存儲表格數據
+        # 創建一個空列表來儲存表格資料
         table_data = []
 
         # 找到表格的標題行
@@ -84,40 +84,40 @@ try:
         # 找到所有的行
         rows = table.find_all('tr')
 
-        # 跳過第一行，即表格的標題行，從第二行開始提取數據
+        # 跳過第一行，即表格的標題行，從第二行開始提取資料
         for row in rows[1:]:
             # 找到每一行中的所有列
             cols1 = row.find_all(['td'])
 
             # 確保列數與表格標題行的列數相同，以避免爬取非表格內容
             if len(cols1,) == len(column_names):
-                # 提取每一列的文本，並去除空白字符
+                # 提取每一列的資料，並去除空白字符
                 cols_data = [col.text.strip() for col in cols1]
-                # 將提取的數據添加到表格數據列表中
+                # 將提取的資料添加到表格資料列表中
                 table_data.append(cols_data)
 
         # 設定 CSV 檔案路徑
         if market_type == '上市':
-            csv_file_path = "tse_stock_info.csv"
+            csv_file_path = "sii_stock_info.csv"
         else:
             csv_file_path = "otc_stock_info.csv"
 
-        # 將表格數據寫入 CSV 文件
+        # 將表格資料寫入 CSV 檔案
         with open(csv_file_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
-            # 使用 csv 模塊寫入 CSV 文件
+            # 使用 csv 模組寫入 CSV 檔案
             writer = csv.writer(csvfile)
             
             # 先寫入標題行
             writer.writerow(column_names)
             
-            # 寫入表格內容，使用 processed_data
+            # 寫入表格內容
             writer.writerows(table_data)  # 將 DataFrame 轉換為列表後寫入
 
-        print("表格數據已成功保存到:", csv_file_path)
+        print("表格資料已成功儲存到:", csv_file_path)
 
         # 檔案路徑
         if market_type == '上市':        
-            # 讀取剛剛存入的 CSV 文件
+            # 讀取剛剛存入的 CSV 檔案
             df = pd.read_csv(csv_file_path)
 
             column_names2= ["公司代號", "公司名稱", "公司簡稱", "產業類別", "住址",  "董事長", "成立日期", "上市日期", "普通股每股面額", "實收資本額(元)", 
@@ -126,13 +126,13 @@ try:
             # 選擇指定的欄位
             selected_columns = df[column_names2]
 
-            # 新的 CSV 文件路徑
-            tse_stock_info_file_path = "tse_stock_info_select.csv"
+            # 新的 CSV 檔案路徑
+            sii_stock_info_file_path = "sii_stock_info_select.csv"
 
-            # 將選擇的資料存入新的 CSV 文件
-            selected_columns.to_csv(tse_stock_info_file_path, index=False, encoding='utf-8-sig')
+            # 將選擇的資料存入新的 CSV 檔案
+            selected_columns.to_csv(sii_stock_info_file_path, index=False, encoding='utf-8-sig')
 
-            print("選擇的資料已成功保存到:", tse_stock_info_file_path)
+            print("選擇的資料已成功儲存到:", sii_stock_info_file_path)
         else:
             df = pd.read_csv(csv_file_path)
             column_names2= ["公司代號", "公司名稱", "公司簡稱", "產業類別", "住址", "董事長", "成立日期", "上櫃日期", "普通股每股面額", "實收資本額(元)", 
@@ -141,13 +141,13 @@ try:
             # 選擇指定的欄位
             selected_columns = df[column_names2]
 
-            # 新的 CSV 文件路徑
+            # 新的 CSV 檔案路徑
             otc_stock_info_file_path = "otc_stock_info_select.csv"
 
-            # 將選擇的資料存入新的 CSV 文件
+            # 將選擇的資料存入新的 CSV 檔案
             selected_columns.to_csv(otc_stock_info_file_path, index=False, encoding='utf-8-sig')
 
-            print("選擇的資料已成功保存到:", otc_stock_info_file_path)
+            print("選擇的資料已成功儲存到:", otc_stock_info_file_path)
 
 finally:
     # 關閉瀏覽器
