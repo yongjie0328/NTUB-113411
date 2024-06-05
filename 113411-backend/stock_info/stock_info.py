b38643a3279a46ace30ecb5ccaadbbcac1f2f401
yinghsiu0701 <94967161+yinghsiu0701@users.noreply.github.com>
2024-06-05 23:03:32 +0800

    Add files via upload

--- 113411-backend/stock_info/stock_info.py
+++ 113411-backend/stock_info/stock_info.py
@@ -1,13 +1,13 @@
-import csv
-import time 
-import pandas as pd
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time 
+import csv
 
 # 設定瀏覽器驅動程式路徑
 driver_path = r'C:\Users\yingh\Desktop\python爬蟲\chromedriver.exe'
@@ -57,7 +57,7 @@ try:
 
         if market_type == '上市':
             # 將表頭欄位名稱指定給 column_names 變數
-            column_names = ["公司代號", "公司名稱", "公司簡稱", "產業類別", "外國企業註冊地國", "住址", "營利事業統一編號", "董事長", "總經理",
+            column_names = ["股票代碼", "公司名稱", "公司簡稱", "產業類別", "外國企業註冊地國", "住址", "營利事業統一編號", "董事長", "總經理",
                             "發言人", "發言人職稱", "代理發言人", "總機電話", "成立日期", "上市日期", "普通股每股面額", "實收資本額(元)",
                             "已發行普通股數或TDR原發行股數", "私募普通股(股)", "特別股(股)", "編製財務報告類型", "普通股盈餘分派或虧損撥補頻率",
                             "普通股年度(含第4季或後半年度)現金股息及紅利決議層級", "股票過戶機構", "過戶電話", "過戶地址", "簽證會計師事務所",
@@ -65,7 +65,7 @@ try:
                             "投資人關係聯絡人職稱", "投資人關係聯絡電話", "投資人關係聯絡電子郵件", "公司網站內利害關係人專區網址"]
         else:
             # 將表頭欄位名稱指定給 column_names 變數
-            column_names = ["公司代號", "公司名稱", "公司簡稱", "產業類別", "外國企業註冊地國", "住址", "營利事業統一編號", "董事長", "總經理",
+            column_names = ["股票代碼", "公司名稱", "公司簡稱", "產業類別", "外國企業註冊地國", "住址", "營利事業統一編號", "董事長", "總經理",
                             "發言人", "發言人職稱", "代理發言人", "總機電話", "成立日期", "上櫃日期", "普通股每股面額", "實收資本額(元)",
                             "已發行普通股數或TDR原發行股數", "私募普通股(股)", "特別股(股)", "編製財務報告類型", "普通股盈餘分派或虧損撥補頻率",
                             "普通股年度(含第4季或後半年度)現金股息及紅利決議層級", "股票過戶機構", "過戶電話", "過戶地址", "簽證會計師事務所",
@@ -90,17 +90,22 @@ try:
             cols1 = row.find_all(['td'])
 
             # 確保列數與表格標題行的列數相同，以避免爬取非表格內容
-            if len(cols1,) == len(column_names):
+            if len(cols1) == len(column_names):
                 # 提取每一列的資料，並去除空白字符
                 cols_data = [col.text.strip() for col in cols1]
+                
+                # 判斷是否為存託憑證資料，如果是，則跳過此次迴圈
+                if "存託憑證" in cols_data[3]:  # 產業類別是資料中的第四列
+                    continue
+                
                 # 將提取的資料添加到表格資料列表中
                 table_data.append(cols_data)
 
         # 設定 CSV 檔案路徑
         if market_type == '上市':
-            csv_file_path = "sii_stock_info.csv"
+            csv_file_path = "./stock_info/sii_stock_info.csv"
         else:
-            csv_file_path = "otc_stock_info.csv"
+            csv_file_path = "./stock_info/otc_stock_info.csv"
 
         # 將表格資料寫入 CSV 檔案
         with open(csv_file_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
@@ -113,41 +118,53 @@ try:
             # 寫入表格內容
             writer.writerows(table_data)  # 將 DataFrame 轉換為列表後寫入
 
-        print("表格資料已成功儲存到:", csv_file_path)
+        print(f'{market_type}資料已成功儲存到', csv_file_path)
+
+        # 讀取剛剛存入的 CSV 檔案
+        df = pd.read_csv(csv_file_path)
+        
+        # 替換 '-' 為 '無'
+        df.replace('-', '無', inplace=True)
+        
+        # 替換 NaN (空值) 為 '無'
+        df.fillna('無', inplace=True)
+
+        # 儲存替換後的資料回到原CSV檔案
+        df.to_csv(csv_file_path, index=False, encoding='utf-8-sig')
 
         # 檔案路徑
         if market_type == '上市':        
             # 讀取剛剛存入的 CSV 檔案
             df = pd.read_csv(csv_file_path)
 
-            column_names2= ["公司代號", "公司名稱", "公司簡稱", "產業類別", "住址",  "董事長", "成立日期", "上市日期", "普通股每股面額", "實收資本額(元)", 
+            sii_column_names= ["股票代碼", "公司名稱", "公司簡稱", "產業類別", "住址",  "董事長", "成立日期", "上市日期", "普通股每股面額", "實收資本額(元)", 
                             "已發行普通股數或TDR原發行股數", "編製財務報告類型", "普通股盈餘分派或虧損撥補頻率", 
                             "普通股年度(含第4季或後半年度)現金股息及紅利決議層級", "股票過戶機構", "簽證會計師事務所", "英文簡稱", "電子郵件信箱", "公司網址"]
             # 選擇指定的欄位
-            selected_columns = df[column_names2]
+            selected_columns = df[sii_column_names]
 
             # 新的 CSV 檔案路徑
-            sii_stock_info_file_path = "sii_stock_info_select.csv"
+            sii_stock_info_file_path = "./stock_info/sii_stock_info_select.csv"
 
             # 將選擇的資料存入新的 CSV 檔案
             selected_columns.to_csv(sii_stock_info_file_path, index=False, encoding='utf-8-sig')
 
-            print("選擇的資料已成功儲存到:", sii_stock_info_file_path)
+            print(f'選擇的{market_type}資料已成功儲存到', sii_stock_info_file_path)
         else:
             df = pd.read_csv(csv_file_path)
-            column_names2= ["公司代號", "公司名稱", "公司簡稱", "產業類別", "住址", "董事長", "成立日期", "上櫃日期", "普通股每股面額", "實收資本額(元)", 
+            otc_column_names= ["股票代碼", "公司名稱", "公司簡稱", "產業類別", "住址", "董事長", "成立日期", "上櫃日期", "普通股每股面額", "實收資本額(元)", 
                             "已發行普通股數或TDR原發行股數", "編製財務報告類型", "普通股盈餘分派或虧損撥補頻率", 
                             "普通股年度(含第4季或後半年度)現金股息及紅利決議層級", "股票過戶機構", "簽證會計師事務所", "英文簡稱", "電子郵件信箱", "公司網址"]
             # 選擇指定的欄位
-            selected_columns = df[column_names2]
+            selected_columns = df[otc_column_names]
 
             # 新的 CSV 檔案路徑
-            otc_stock_info_file_path = "otc_stock_info_select.csv"
+            otc_stock_info_file_path = "./stock_info/otc_stock_info_select.csv"
 
             # 將選擇的資料存入新的 CSV 檔案
             selected_columns.to_csv(otc_stock_info_file_path, index=False, encoding='utf-8-sig')
 
-            print("選擇的資料已成功儲存到:", otc_stock_info_file_path)
+            print(f'選擇的{market_type}資料已成功儲存到', otc_stock_info_file_path)
 
 finally:
     # 關閉瀏覽器
