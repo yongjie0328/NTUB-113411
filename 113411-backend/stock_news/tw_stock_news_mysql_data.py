3e45ba8073b9de4f82717e55f4e5b8069e56185b
yinghsiu0701 <94967161+yinghsiu0701@users.noreply.github.com>
2024-06-06 00:03:09 +0800

    Add files via upload

---
+++ 113411-backend/stock_news/tw_stock_news_mysql_data.py
@@ -0,0 +1,56 @@
+import mysql.connector
+import pandas as pd
+
+# 讀取 CSV 檔案到 DataFrame
+df = pd.read_csv(r"C:\Users\yingh\Desktop\python爬蟲\stock_news\tw_stock_processed.csv")
+
+# MySQL連線設置
+host = '140.131.114.242'
+port=3306
+user = 'SA411'
+password = '@Zxcll34ll'
+database = '113-113411'
+
+# 建立MySQL連線
+conn = mysql.connector.connect(host=host, port=port, user=user, password=password, database=database)
+
+# 創建一個MySQL cursor物件
+cursor = conn.cursor()
+
+# 定義中文到英文的欄位名稱對照
+column_mapping = {
+    "新聞編號": "news_id",
+    "網址": "url",
+    "標題": "title",
+    "摘要": "summary",
+    "關鍵字": "keyword",
+    "內容": "content",
+    "發布時間": "date",
+    "類別": "category_name",
+    "類別號碼": "category_id",
+}
+
+# 寫入資料到MySQL資料表
+for index, row in df.iterrows():
+    # 將欄位名稱對照為英文
+    mapped_row = {column_mapping[key]: value for key, value in row.items()}
+
+    # 建構寫入資料的SQL語句
+    sql = "INSERT INTO tw_stock_news ({}) VALUES ({})".format(
+        ", ".join(mapped_row.keys()), ", ".join(["%s"] * len(mapped_row))
+    )
+    
+    # 獲取每一行的資料，並轉換為元組(數組)
+    data = tuple(mapped_row.values())
+    
+    # 執行SQL語句
+    cursor.execute(sql, data)
+
+# 提交資料
+conn.commit()
+
+# 關閉MySQL連線
+cursor.close()
+conn.close()
+
+print("資料已成功寫入到MySQL資料表中。")
